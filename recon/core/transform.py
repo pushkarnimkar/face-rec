5a6e63f9d2bc587a02af7a3d371021e56d6ef989
Pushkar Nimkar <pushkar@intangles.com>
2019-03-15 17:24:31 +0530

    [ TRANSFORM ] changes to transformation code

--- recon/core/transform.py
+++ recon/core/transform.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from face_recognition import face_encodings, face_locations
 from typing import Optional, Tuple, Union
 
@@ -7,6 +8,20 @@ import hashlib
 import numpy as np
 
 
+class TransformFailure(Enum):
+    INVALID_JPEG = "INVALID_JPEG"
+    FACE_DETECTION_FAILED = "FACE_DETECTION_FAILED"
+
+
+TRANSFORM_RETT = Union[Tuple[np.ndarray, np.ndarray, np.ndarray, str],
+                       None, TransformFailure]
+
+
+class TransformError(TypeError):
+    def __init__(self, code: TransformFailure):
+        self.code = code
+
+
 def transform_image(image_input: Union[str, bytes]) -> np.ndarray:
     """
     Handles transformation of image in various formats like jpeg, png, etc
@@ -14,20 +29,26 @@ def transform_image(image_input: Union[str, bytes]) -> np.ndarray:
 
     Parameters
     ----------
-    image_input: Image in base64 string or bytes format
+    image_input : Image in base64 string or bytes format
 
     Returns
     -------
     numpy array of R G B values of shape (height, width, 3)
+
+    Raises
+    ------
+    TypeError : When input is not a valid jpeg buffer
 
     """
     buffer = (base64.b64decode(image_input)
               if isinstance(image_input, str) else image_input)
     image_compressed = np.frombuffer(buffer, np.int8)
     image_bgr = cv2.imdecode(image_compressed, cv2.IMREAD_COLOR)
-    image_array = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-
-    return image_array
+    if image_bgr is not None:
+        image_array = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+        return image_array
+    else:
+        raise TransformError(TransformFailure.INVALID_JPEG)
 
 
 def extract_features(image: np.ndarray, face_location_model: str="hog") -> \
@@ -52,44 +73,49 @@ def extract_features(image: np.ndarray, face_location_model: str="hog") -> \
     """
     bbox = np.array(face_locations(image, model=face_location_model))
     if bbox.shape[0] == 0:
-        return
+        raise TransformError(TransformFailure.FACE_DETECTION_FAILED)
     area = np.abs((bbox[:, 0] - bbox[:, 2]) *
                   (bbox[:, 1] - bbox[:, 3]))
     image_hash = hashlib.md5(image).hexdigest()
     face_box = bbox[np.argmax(area), :]
     face_encoding = face_encodings(image, [face_box])[0]
-
     return image_hash, face_box, face_encoding
 
 
-def transform(image_input: Union[str, bytes],
-              face_location_model: str="hog") -> \
-        Optional[Tuple[np.ndarray, np.ndarray, np.ndarray, str]]:
+def transform(image_input: Union[str, bytes], face_location_model: str="hog",
+              error_code: bool=False) -> TRANSFORM_RETT:
     """
     Transforms image buffer into face parameters and hash of of the image.
     If fails to find face, returns None.
 
     Parameters
     ----------
-    image_input: Image in base64 string or bytes format
-    face_location_model: vector encoding of face as per
-        `face_recognition` library
+    image_input : (string | bytes)
+        Image in base64 string or bytes format
+    face_location_model : str, default "hog"
+        Model for face detection used by `face_recognition` library
+    error_code : bool, default false
+        If this is set returns error code on failure instead of `None`
 
     Returns
     -------
-    image: numpy array of image R G B value
-    face_encodings: vector encoding of face as per
-        `face_recognition` library
-    face_box: coordinates of bounding box of face in image (x1, y1, x2, y2)
-    image_hash: MD5 hash of numpy array of image
+    image : np.ndarray, success
+        Image as an array of shape: (width, height, channels)
+    face_encodings : np.ndarray, success
+        Vector encoding of face as per `face_recognition` library
+    face_box : np.ndarray, success
+        Coordinates of bounding box of face in image (x1, y1, x2, y2)
+    image_hash : str, success
+        MD5 hash of numpy array of image
+    error_code : (None | TransformFailure)
+        Code describing reason of failure if `error_code` is set in input,
+        else returns `None`.
 
     """
     try:
         image = transform_image(image_input)
-        image_hash, face_box, face_encoding = \
-            extract_features(image, face_location_model=face_location_model)
+        image_hash, face_box, face_encoding = extract_features(
+            image, face_location_model=face_location_model)
         return image, face_encoding, face_box, image_hash
-    except TypeError:
-        return None
-    except cv2.error:
-        return None
+    except TransformError as te:
+        return te.code if error_code else None
